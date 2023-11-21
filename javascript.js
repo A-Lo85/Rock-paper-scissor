f59a1fb6262372f5b593d96d4be76c9724b6d226
alo85 <alochatman465@gmail.com>
2023-11-20 22:56:46 -0800

    finished the random output function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
 function getComputerChoice(){
+   let go = Math.floor(Math.random() * 3);
+   let shoot ="";
+   if(go === 2){
+        return("Rock");
+    }else if (go == 1){
+        return ("Paper");
+    }else{
+        return("Scissors");
+    }
+}
+
+
+function readySetGo(playerChoice,computerChoice){
     
 }
