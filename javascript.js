8ab41cf49942ec5b4cf06559b48678426aa3ce9f
alo85 <alochatman465@gmail.com>
2023-11-20 23:30:36 -0800

    Fixed the spelling of the each if statments

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 function getComputerChoice(){
    let go = Math.floor(Math.random() * 3);
    let shoot ="";
-   if(go === 2){
+   if(go == 2){
         return("Rock");
     }else if (go == 1){
         return ("Paper");
-    }else{
+    }else if(go == 0){
         return("Scissors");
     }
 }
@@ -14,28 +14,27 @@ function getComputerChoice(){
 function readySetGo(playerChoice,computerChoice){
     if (playerChoice == "Rock" && computerChoice =="Paper"){
           return("You lost, Paper beats rock")
-    }else if(playerChoice == "Rock" && computerChoice =="Scissor"){
-        return("You Won, rock beats Scissor")
+    }else if(playerChoice == "Rock" && computerChoice =="Scissors"){
+         return("You Won, rock beats Scissor")
     }else if(playerChoice == "Rock" && computerChoice =="Rock"){
-        return("You tied, Rock ties Rock")
+         return("You tied, Rock ties Rock") 
+    }
+    
+    
+    else if (playerChoice == "Paper" && computerChoice =="Paper"){
+         return("You tied, paper can't beat paper") 
+    }else if(playerChoice == "Paper" && computerChoice =="Scissors"){
+         return("You lost, scissor beats paper")
+    }else if(playerChoice == "Paper" && computerChoice =="Rock"){
+         return("You Won, paper beats rock")
+    } 
 
-}if (playerChoice == "Paper" && computerChoice =="Paper"){
-    return("You tied, paper can't beat paper")
-}else if(playerChoice == "Paper" && computerChoice =="Scissor"){
-  return("You lost, scissor beats paper")
-}else if(playerChoice == "Paper" && computerChoice =="Rock"){
-  return("You Won, paper beats rock")
-}
-
-if (playerChoice == "Scissors" && computerChoice =="Paper"){
-    return("You lost, Paper beats rock")
-}else if(playerChoice == "Scissor" && computerChoice =="Scissor"){
-  return("You tied, scissor can't beat scissor")
-}else if(playerChoice == "Scissors" && computerChoice =="Rock"){
-  return("You lost, Rock beats scissor")
-}
-
-
-
+     else if (playerChoice == "Scissors" && computerChoice =="Paper"){
+        return("You lost, Paper beats rock")
+    }else if(playerChoice == "Scissors" && computerChoice =="Scissors"){
+         return("You tied, scissor can't beat scissor")
+    }else if(playerChoice == "Scissors" && computerChoice =="Rock"){
+        return("You lost, Rock beats scissor")
+    }
 }
 
