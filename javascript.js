93db25d0b226bc3d84339bbe4b9198e5f5765ea0
alo85 <alochatman465@gmail.com>
2023-11-20 23:19:18 -0800

    computer vs human fucntion paper section

--- javascript.js
+++ javascript.js
@@ -18,7 +18,16 @@ function readySetGo(playerChoice,computerChoice){
         return("You Won, rock beats Scissor")
     }else if(playerChoice == "Rock" && computerChoice =="Rock"){
         return("You tied, Rock ties Rock")
+        
+}if (playerChoice == "Paper" && computerChoice =="Paper"){
+    return("You tied, paper can't beat paper")
+}else if(playerChoice == "Paper" && computerChoice =="Scissor"){
+  return("You lost, scissor beats paper")
+}else if(playerChoice == "Paper" && computerChoice =="Rock"){
+  return("You Won, paper beats rock")
 }
+
+
 
 
 }
